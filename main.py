a96bc93b447e59eeb180a29afb110b1f8f79ced3
대현 <rkdeown10@naver.com>
2022-05-31 15:58:15 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ def file_writer():
 
 def record():
     global count, zed, runtime, status, next_frame
+
     # Mat
     left = sl.Mat()
     right = sl.Mat()
@@ -65,29 +66,22 @@ def record():
     dis = sl.Mat()
 
     while True:
-        if status:  # 스페이스바를 누를떼
+        if status:
+
             if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
-                path = os.path.join(save_path, frm_path)
-                # set data
                 zed.retrieve_image(left, sl.VIEW.LEFT)  # images
                 zed.retrieve_image(right, sl.VIEW.RIGHT)
                 zed.retrieve_image(depth, sl.VIEW.DEPTH)
                 zed.retrieve_measure(dis, sl.MEASURE.DEPTH)  # distance
-                zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.CURRENT)  # 기울기 센서
-
-                # get data
-                left = left.get_data()
-                right = right.get_data()
-                depth = depth.get_data()
-                dis = distance_undefined(dis.get_data())
-                dis = DepthNormalizing(dis)
+                path = os.path.join(save_path, frm_path)
+                zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.CURRENT)
                 quaternion = sensors_data.get_imu_data().get_pose().get_orientation().get()
 
                 # Save image
-                que.put({'left': [os.path.join(path, f'left_{str(count).zfill(4)}.jpg'), left]})
-                que.put({'right': [os.path.join(path, f'right_{str(count).zfill(4)}.jpg'), right]})
-                que.put({'depth': [os.path.join(path, f'depth_{str(count).zfill(4)}.jpg'), depth]})
-                que.put({'dis': [os.path.join(path, f'distance_{str(count).zfill(4)}.npy'), dis]})
+                que.put({'left': [os.path.join(path, f'left_{str(count).zfill(4)}.jpg'), left.get_data()]})
+                que.put({'right': [os.path.join(path, f'right_{str(count).zfill(4)}.jpg'), right.get_data()]})
+                que.put({'depth': [os.path.join(path, f'depth_{str(count).zfill(4)}.jpg'), depth.get_data()]})
+                que.put({'dis': [os.path.join(path, f'distance_{str(count).zfill(4)}.npy'), DepthNormalizing(distance_undefined(dis.get_data()))]})
                 que.put({'slope': [os.path.join(path, f'slope_{str(count).zfill(4)}.npy'), quaternion]})
             count += 1
         else:
@@ -98,10 +92,10 @@ def main():
     global count, zed, runtime, status, next_frame, frm_path
     err = zed.open(runtime)
     if err != sl.ERROR_CODE.SUCCESS:
-        print('카메라가 연결되어있지 않거나, 이미 켜져있습니다')
+        print('카메라가 연결되어있지 않던가 이미 카메라가 켜져있습니다')
         exit()
     else:
-        print('카메라 연결 성공!')
+        print('카메라 연결성공!')
 
     runtime = sl.RuntimeParameters()
     runtime.sensing_mode = sl.SENSING_MODE.FILL
@@ -109,7 +103,6 @@ def main():
     left = sl.Mat()
     # right = sl.Mat()
     # depth = sl.Mat()
-
     # Get image
     image_size = zed.get_camera_information().camera_resolution
     print(f'original width : {image_size.width}px\noriginal height : {image_size.height}px')
@@ -133,11 +126,8 @@ def main():
             # resized_depth = cv2.resize(depth.get_data(), (image_size.width, image_size.height))
 
             cv2.putText(resized_left, str(count), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
-
-            # cv2.putText(resized_depth, str(count), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
             # cv2.putText(resized_left, 'Zed Left', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
             # cv2.putText(resized_right, 'Zed Right', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
-
             cv2.imshow('ZED_left', resized_left)
             # cv2.imshow('ZED_right', resized_right)
             # cv2.imshow('ZED_depth', resized_depth)
