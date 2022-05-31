3d12a7b1c4ed5737bb3f8af5df3152f5beed6386
대현 <rkdeown10@naver.com>
2022-05-31 15:59:29 +0900

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,9 +1,7 @@
 set root=C:\Users\vitasoft\anaconda3
 call %root%\Scripts\activate.bat %root%
 
-call conda env list
 call conda activate Zed
-call cd C:\Users\vitasoft\Desktop\ZedProject
 call python main.py
 
 pause
