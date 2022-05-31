85d1caf817e7ff0c577ab5cc10ae0c220a4b34f6
daehyuh <rkdeown10@naver.com>
2022-05-31 16:38:26 +0900

    last commit

--- main.py
+++ main.py
@@ -18,16 +18,13 @@ runtime.coordinate_units = sl.UNIT.METER
 runtime.depth_minimum_distance = 0.15
 runtime.depth_maximum_distance = 40
 runtime.depth_mode = sl.DEPTH_MODE.ULTRA  # depth 해상도 ULTRA
-
 sensors_data = sl.SensorsData()
+que = Queue()
 
 status = False  # 키 입력 확인
-next_frame = False  # 카메라 확인
 save_path = 'saved_img'  # 저장 경로
 frm_path = ''  # 저장 날짜
 count = 0  # 저장 파일 카운트
-
-que = Queue()
 
 
 def DepthNormalizing(data):
@@ -37,7 +34,6 @@ def DepthNormalizing(data):
 
 
 def distance_undefined(nd_array):
-    # nd_array[np.isnan(nd_array)] = 0
     nd_array[nd_array == inf] = 0
     nd_array[nd_array == -inf] = 0
     np.nan_to_num(nd_array)
@@ -57,7 +53,7 @@ def file_writer():
 
 
 def record():
-    global count, zed, runtime, status, next_frame
+    global que, count, zed, runtime, status
 
     # Mat
     left = sl.Mat()
@@ -67,74 +63,68 @@ def record():
 
     while True:
         if status:
-
             if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
                 zed.retrieve_image(left, sl.VIEW.LEFT)  # images
                 zed.retrieve_image(right, sl.VIEW.RIGHT)
                 zed.retrieve_image(depth, sl.VIEW.DEPTH)
-
                 zed.retrieve_measure(dis, sl.MEASURE.DEPTH)  # distance
-
                 path = os.path.join(save_path, frm_path)
-
                 zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.CURRENT)
                 quaternion = sensors_data.get_imu_data().get_pose().get_orientation().get()
-                print(type(quaternion))
 
                 # Save image
                 que.put({'left': [os.path.join(path, f'left_{str(count).zfill(4)}.jpg'), left.get_data()]})
                 que.put({'right': [os.path.join(path, f'right_{str(count).zfill(4)}.jpg'), right.get_data()]})
                 que.put({'depth': [os.path.join(path, f'depth_{str(count).zfill(4)}.jpg'), depth.get_data()]})
-                que.put({'dis': [os.path.join(path, f'distance_{str(count).zfill(4)}.npy'), DepthNormalizing(distance_undefined(dis.get_data()))]})
+                que.put({'dis': [os.path.join(path, f'distance_{str(count).zfill(4)}.npy'),
+                                 DepthNormalizing(distance_undefined(dis.get_data()))]})
                 que.put({'slope': [os.path.join(path, f'slope_{str(count).zfill(4)}.npy'), quaternion]})
-            count += 1
+                count += 1
         else:
             count = 0
 
 
 def main():
-    global count, zed, runtime, status, next_frame, frm_path
+    global count, zed, runtime, status, frm_path
     err = zed.open(runtime)
     if err != sl.ERROR_CODE.SUCCESS:
-        print('camera opened failed')
+        print('카메라가 연결되어있지 않던가 이미 카메라가 켜져있습니다')
         exit()
     else:
-        print('Camera Opened!')
+        print('카메라 연결성공!')
 
     runtime = sl.RuntimeParameters()
     runtime.sensing_mode = sl.SENSING_MODE.FILL
 
     left = sl.Mat()
-    right = sl.Mat()
-    depth = sl.Mat()
+    # right = sl.Mat()
+    # depth = sl.Mat()
     # Get image
     image_size = zed.get_camera_information().camera_resolution
-    print(f'original width : {image_size.width}px\noriginal height : {image_size.height}px')
     # image resizing
     image_size.width = image_size.width / 2.5
     image_size.height = image_size.height / 2.5
-    print(f'resized width : {image_size.width}px\nresized height : {image_size.height}px')
 
+    print("\n스페이스바: 촬영을 시작/종료\nESC : 프로그램종료\n")
     while True:
 
         if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
-            next_frame = True
 
             # Show Camera
             zed.retrieve_image(left, sl.VIEW.LEFT)
-            zed.retrieve_image(right, sl.VIEW.RIGHT)
-            zed.retrieve_image(depth, sl.VIEW.DEPTH)
+            # zed.retrieve_image(right, sl.VIEW.RIGHT)
+            # zed.retrieve_image(depth, sl.VIEW.DEPTH)
 
             resized_left = cv2.resize(left.get_data(), (image_size.width, image_size.height))
-            resized_right = cv2.resize(right.get_data(), (image_size.width, image_size.height))
-            resized_depth = cv2.resize(depth.get_data(), (image_size.width, image_size.height))
+            # resized_right = cv2.resize(right.get_data(), (image_size.width, image_size.height))
+            # resized_depth = cv2.resize(depth.get_data(), (image_size.width, image_size.height))
 
-            cv2.putText(resized_depth, str(count), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
-            cv2.putText(resized_left, 'Zed Left', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
-            cv2.putText(resized_right, 'Zed Right', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
+            cv2.putText(resized_left, str(count), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
+            # cv2.putText(resized_left, 'Zed Left', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
+            # cv2.putText(resized_right, 'Zed Right', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
             cv2.imshow('ZED_left', resized_left)
-            cv2.imshow('ZED_right', resized_right)
-            cv2.imshow('ZED_depth', resized_depth)
+            # cv2.imshow('ZED_right', resized_right)
+            # cv2.imshow('ZED_depth', resized_depth)
 
         else:
             cv2.destroyAllWindows()
@@ -151,10 +141,13 @@ def main():
                 root_path = os.path.join(save_path, frm_path)
                 os.makedirs(root_path, exist_ok=True)  # image dir
                 os.makedirs(root_path + "/video", exist_ok=True)  # video dir
+                print(f"녹화시작 {root_path}에 파일이 저장됩니다.")
             else:
                 # print(root_path)
-                os.system(
-                    "ffmpeg -f image2 -r 5 -i " + root_path + "/depth_%04d.jpg -vcodec mpeg4 -y " + root_path + "/video/depth_images_convert_video.mp4")
+                # os.system(
+                #     "ffmpeg -f image2 -r 5 -i " + root_path + "/depth_%04d.jpg -vcodec mpeg4 -y " + root_path + "/video/depth_images_convert_video.mp4")
+                print(f"{count}개의 파일이 {root_path}에 저장되었습니다.")
+                print("\n스페이스바를 눌러 촬영을 시작/종료 하세요\nESC : 프로그램종료\n")
                 count = 0
                 frm_path = ''
 
